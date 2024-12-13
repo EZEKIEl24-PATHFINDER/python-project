92d3b70dc312e276d6a0086d9edf4d7d764e786f
EZEKIEL24-PATHFINDER <ezekielapribo@gmail.com>
2024-12-13 16:42:30 +0100

    new

---
+++ 3mtt assignment 1.py
@@ -0,0 +1,16 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Type "help", "copyright", "credits" or "license()" for more information.
+SyntaxError: invalid syntax
+>>> x=1,2,3,4,5,6
+>>> print(x)
+(1, 2, 3, 4, 5, 6)
+>>> sum(x)
+21
+>>> average=sum(x)/6
+>>> print(average)
+3.5
+>>> min(x)
+1
+>>> max(x)
+6
